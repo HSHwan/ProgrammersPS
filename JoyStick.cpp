ee8cc3f70c30888727581fd1bcc4a18a6885d80c
HSHwan <tnghks2866@gmail.com>
2024-02-16 14:45:10 +0900

    JoyStick solved

--- JoyStick.cpp
+++ JoyStick.cpp
@@ -1,7 +1,17 @@
-#include <iostream>
+#include <string>
+#include <vector>
+#include <algorithm>
 
 using namespace std;
 
-int main(){
-    cout << 'Z' - 'A';
+int solution(string name) {
+    int answer = 0, name_len = name.length(), move_cnt = name_len - 1, next_idx;
+    for (int i = 0; i < name_len; i++){
+        answer += min(name[i] - 'A', 'Z' - name[i] + 1);
+        next_idx = i + 1;
+        while (next_idx < name_len && name[next_idx] == 'A')   next_idx++;
+        move_cnt = min(move_cnt, i + name_len - next_idx + min(i, name_len - next_idx));
+    }
+    answer += move_cnt;
+    return answer;
 }
