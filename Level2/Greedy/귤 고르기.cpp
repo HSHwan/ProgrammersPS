0318067241865bdf9cb39e83ddf238a485f825da
HSHwan <tnghks2866@gmail.com>
2024-03-09 21:23:46 +0900

    Refactor: container Map -> vector

--- Level2/Greedy/귤 고르기.cpp
+++ Level2/Greedy/귤 고르기.cpp
@@ -1,23 +1,18 @@
 #include <string>
 #include <vector>
-#include <map>
 #include <algorithm>
 
 using namespace std;
 
 int solution(int k, vector<int> tangerine) {
-    int answer = 0, tangerine_cnt = k;
-    map<int, int> all_tangerines;
+    int answer = 0, tangerine_cnt = k, maxTangerineSize = *max_element(tangerine.begin(), tangerine.end());
+    vector<int> all_tangerines(maxTangerineSize + 1, 0);
     for (const int& tang : tangerine){
-        if (all_tangerines.count(tang) > 0)
-            all_tangerines[tang]++;
-        else
-            all_tangerines[tang] = 1;
+        all_tangerines[tang]++;
     }
-    vector<pair<int, int>> tangerine_sorted(all_tangerines.begin(), all_tangerines.end());
-    sort(tangerine_sorted.begin(), tangerine_sorted.end(), [](const auto& a, const auto& b){ return a.second > b.second;});
-    for (const auto& tang_cnt : tangerine_sorted){
-        tangerine_cnt -= tang_cnt.second;
+    sort(all_tangerines.begin(), all_tangerines.end(), greater<int>());
+    for (const int& tang_cnt : all_tangerines){
+        tangerine_cnt -= tang_cnt;
         answer++;
         if (tangerine_cnt <= 0)
             break;
